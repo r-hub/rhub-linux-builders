b9e7e0fed4813b0b7c8c928d5727e81041a1fd5d
Gabor Csardi <csardi.gabor@gmail.com>
2016-09-05 01:28:21 -0400

    Ubuntu + r-patched + do not build r-release by default

--- jenkins.sh
+++ jenkins.sh
@@ -2,10 +2,21 @@
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
+# We don't build the relase versions periodically, these are
+# done manually, as needed.
+
 ./build-image.sh debian-gcc         || true
 ./build-image.sh debian-gcc-devel   || true
+# ./build-image.sh debian-gcc-release || true
+./build-image.sh debian-gcc-patched || true
+
 ./build-image.sh fedora-clang       || true
 ./build-image.sh fedora-clang-devel || true
 ./build-image.sh fedora-gcc         || true
 ./build-image.sh fedora-gcc-devel   || true
 ./build-image.sh centos6-epel       || true
+
+./build-image.sh ubuntu             || true
+./build-image.sh ubuntu-gcc         || true
+./build-image.sh ubuntu-gcc-devel   || true
+# ./build-image.sh ubuntu-gcc-release || true
