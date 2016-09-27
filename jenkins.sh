db7273e43620a401efdc3c9446587aa66653bef3
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-27 09:58:40 +0100

    Clean up unused docker images and containers

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,10 @@
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
+# Clean up unused Docker images and containers
+docker images  | grep -v latest | awk ' { print $3; } ' | xargs docker rmi || true
+docker ps -a | grep 'Exited' |  awk ' { print $1; } ' | xargs docker rm || true
+
 # We don't build the relase versions periodically, these are
 # done manually, as needed.
 
