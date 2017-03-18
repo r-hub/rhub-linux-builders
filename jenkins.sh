21bc7de296e9e0dc7fb56e5559bfff1bf8b04eb1
Gábor Csárdi <csardi.gabor@gmail.com>
2017-03-18 13:42:07 +0000

    Fail jenkins job if a build fails

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS   || exit 1
+
+# Do not want to exit on error now
+set +e
 
 # Clean up unused Docker images and containers
 docker images  | grep -v latest | awk ' { print $3; } ' | xargs docker rmi || true
@@ -9,22 +12,25 @@ docker ps -a | grep 'Exited' |  awk ' { print $1; } ' | xargs docker rm || true
 # We don't build the relase versions periodically, these are
 # done manually, as needed.
 
-./build-image.sh debian-gcc                    || true
-./build-image.sh debian-gcc-devel   --no-cache || true
-./build-image.sh debian-gcc-release            || true
-./build-image.sh debian-gcc-patched --no-cache || true
+failed=0
+./build-image.sh debian-gcc                    || failed=1
+./build-image.sh debian-gcc-devel   --no-cache || failed=1
+./build-image.sh debian-gcc-release            || failed=1
+./build-image.sh debian-gcc-patched --no-cache || failed=1
 
-./build-image.sh fedora-clang                  || true
-./build-image.sh fedora-clang-devel --no-cache || true
-./build-image.sh fedora-gcc                    || true
-./build-image.sh fedora-gcc-devel   --no-cache || true
+./build-image.sh fedora-clang                  || failed=1
+./build-image.sh fedora-clang-devel --no-cache || failed=1
+./build-image.sh fedora-gcc                    || failed=1
+./build-image.sh fedora-gcc-devel   --no-cache || failed=1
 
-./build-image.sh centos6-epel                  || true
-./build-image.sh centos6-epel-rdt              || true
+./build-image.sh centos6-epel                  || failed=1
+./build-image.sh centos6-epel-rdt              || failed=1
 
-./build-image.sh ubuntu                        || true
-./build-image.sh ubuntu-gcc                    || true
-./build-image.sh ubuntu-gcc-devel   --no-cache || true
-./build-image.sh ubuntu-gcc-release            || true
+./build-image.sh ubuntu                        || failed=1
+./build-image.sh ubuntu-gcc                    || failed=1
+./build-image.sh ubuntu-gcc-devel   --no-cache || failed=1
+./build-image.sh ubuntu-gcc-release            || failed=1
 
-./build-image.sh rocker-gcc-san     --no-cache || true
+./build-image.sh rocker-gcc-san     --no-cache || failed=1
+
+exit $failed
