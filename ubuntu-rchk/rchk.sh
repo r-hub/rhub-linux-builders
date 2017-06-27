11ca3852ddce5d66627b6a3f88259681f81ea97e
Gábor Csárdi <csardi.gabor@gmail.com>
2017-06-27 14:01:24 +0000

    First version of rchk container

---
+++ ubuntu-rchk/rchk.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+## We restore R_LIBS
+ORIGRLIBS="$R_LIBS"
+. /opt/rchk/scripts/cmpconfig.inc
+export R_LIBS="$ORIGRLIBS"
+
+${RBINARY} CMD INSTALL "$1"
+pkg=$(echo $1 | sed 's/_.*$//')
+
+cd /opt/R-svn
+/opt/rchk/scripts/check_package.sh ${pkg}
+
+cat packages/lib/${pkg}/libs/${pkg}.so.bcheck
+cat packages/lib/${pkg}/libs/${pkg}.so.maacheck
