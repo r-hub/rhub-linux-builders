8064a91a25ffb3101f5581dd2c0959914bdea21b
Gábor Csárdi <csardi.gabor@gmail.com>
2021-01-26 10:33:16 +0000

    Update Ubuntu version to 20.04

--- ubuntu-rchk/rchk.sh
+++ ubuntu-rchk/rchk.sh
@@ -2,16 +2,16 @@
 
 ## We restore R_LIBS
 ORIGRLIBS="$R_LIBS"
-cd /opt/R-svn
-. /opt/rchk/scripts/cmpconfig.inc
+cd /home/docker/R-svn
+. /home/docker/rchk/scripts/cmpconfig.inc
 cd -
 export R_LIBS="$ORIGRLIBS"
 
 ${RBINARY} CMD INSTALL "$1"
 pkg=$(echo $1 | sed 's/_.*$//')
 
-cd /opt/R-svn
-/opt/rchk/scripts/check_package.sh ${pkg}
+cd /home/docker/R-svn
+/home/docker/rchk/scripts/check_package.sh ${pkg}
 
 cat packages/lib/${pkg}/libs/${pkg}.so.bcheck
 cat packages/lib/${pkg}/libs/${pkg}.so.maacheck
